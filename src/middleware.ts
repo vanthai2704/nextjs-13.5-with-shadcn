c7f21c206ff39163d475902d1364c3c844c9cbad
Nguyen Van Thai <>
2023-10-22 19:17:24 +0700

    update on production

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,9 +15,9 @@ export default withAuth(
 
     const cookies = req.cookies;
 
-    console.log("cookies ====== ", cookies.get("next-auth.csrf-token"));
+    console.log("cookies ====== ", cookies.get("__Host-next-auth.csrf-token"));
 
-    if (!cookies.get("next-auth.csrf-token")) {
+    if (!cookies.get("__Host-next-auth.csrf-token")) {
       return NextResponse.redirect(new URL("/error", req.url));
     }
 
