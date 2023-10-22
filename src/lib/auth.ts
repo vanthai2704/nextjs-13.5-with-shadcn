caee2e1a1cc65d1ca9f8e359ab0b2047c261726b
Nguyen Van Thai <>
2023-10-22 16:41:36 +0700

    test sameSite

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -66,4 +66,29 @@ export const authOptions: NextAuthOptions = {
   pages: {
     signIn: "/login",
   },
+  cookies: {
+    sessionToken: {
+      name: `next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "strict",
+        path: "/",
+      },
+    },
+    callbackUrl: {
+      name: `next-auth.callback-url`,
+      options: {
+        sameSite: "strict",
+        path: "/",
+      },
+    },
+    csrfToken: {
+      name: `next-auth.csrf-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "strict",
+        path: "/",
+      },
+    },
+  },
 };
